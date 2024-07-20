b8151b502fc1c83ac4a6575d573de5dc6e7c91c2
greybaca <greybaca@21-school.ru>
2024-07-20 12:29:22 +0500

    3 constructors with test covered

--- src/containers/s21_vector.h
+++ src/containers/s21_vector.h
@@ -16,6 +16,20 @@ namespace s21 {
             vector(vector &&v); //move constructor
             ~vector(); //destructor
             // operator=(vector &&v); //assignment operator overload for moving object
+
+            //getters block
+            value_type *get_data() {
+                return data;
+            }
+
+            size_type get_size() {
+                return size;
+            }
+
+            size_type get_capacity() {
+                return capacity;
+            }
+            //end of getters block
     
         private:
             value_type *data;
@@ -24,21 +38,46 @@ namespace s21 {
     };
 
     template <typename T> //default contructor
-    inline vector<T>::vector(): data(nullptr), size(0), capacity(0) {}
+    inline vector<T>::vector(): data(nullptr), size(0), capacity(0) {
+        std::cout << "\033[1;103m called default constructor \033[0m \n";  //String for deubug, need to delete
+    }
 
     template <typename T> //constructor with size and initializated by zero
     inline vector<T>::vector(size_type n) {
+        std::cout << "\033[1;103m called constructor with size \033[0m \n";  //String for deubug, need to delete
         data = new value_type[n]();
         if (data == nullptr) std::__throw_bad_alloc();
         size = n;
         capacity = n;
     }
 
-    template <typename T>
+    template <typename T> //constructor from initializer list
     inline vector<T>::vector(std::initializer_list<value_type> const &items) {
+        std::cout << "\033[1;103m called constructor with initializer_list \033[0m \n"; //String for deubug, need to delete
         data = new value_type[items.size()];
         size = items.size();
         capacity = items.size();
+        for (size_t i = 0; i < items.size(); i++) {
+            data[i] = *(items.begin() + i);
+        }
     }
 
+    template <typename T> //copy constructor
+    inline vector<T>::vector(const vector &v): size(v.size), capacity(v.capacity) {
+        std::cout << "\033[1;103m called copy constructor \033[0m \n";  //String for deubug, need to delete
+        this->data = new value_type[v.size];
+        for (size_t i = 0; i < v.size; i++) {
+            this->data[i] = v.data[i];
+        }
+    }
+
+    template <typename T> //move constructor
+    inline vector<T>::vector(vector &&v) {
+        std::move(v);
+    }
+
+    template <typename T>
+    inline vector<T>::~vector() {
+
+    }
 }
--- tests/functional_tests/vector.cpp
+++ tests/functional_tests/vector.cpp
@@ -1,5 +1,40 @@
 #include "../test.h"
 
-TEST(ConstructorTest, Test1) {
-    EXPECT_EQ(1, 1);
+TEST(ConstructorTest, default_constructor_test1) {
+    s21::vector<int> test_vector;
+    EXPECT_TRUE(test_vector.get_data() == nullptr);
+    EXPECT_TRUE(test_vector.get_size() == 0);
+    EXPECT_TRUE(test_vector.get_capacity() == 0);
+}
+
+TEST(ConstructorTest, constructor_with_size_test1) {
+    s21::vector<int> test_vector(5);
+    EXPECT_TRUE(test_vector.get_data() != nullptr);
+    EXPECT_TRUE(test_vector.get_size() == 5);
+    EXPECT_TRUE(test_vector.get_capacity() == 5);
+    for (size_t i = 0; i < 5; i++) {
+        EXPECT_TRUE(*(test_vector.get_data() + i) == 0);
+    }
+}
+
+TEST(ConstructorTest, constructor_from_initializer_list_test1) {
+    s21::vector<int> test_vector{1,2,3,4,5,6,7,8};
+    EXPECT_TRUE(test_vector.get_data() != nullptr);
+    EXPECT_TRUE(test_vector.get_size() == 8);
+    EXPECT_TRUE(test_vector.get_capacity() == 8);
+    for (int i = 0; i < 8; i++) {
+        EXPECT_TRUE(*(test_vector.get_data() + i) == i + 1);
+    }
+}
+
+TEST(ConstructorTest, copy_constructor_test1) {
+    s21::vector<int> test_vector{1,2,3,4,5,6,7,8};
+    s21::vector<int> test_vector2(test_vector);
+    EXPECT_TRUE(test_vector.get_data() != nullptr);
+    EXPECT_TRUE(test_vector.get_data() != test_vector2.get_data());
+    EXPECT_TRUE(test_vector.get_size() == test_vector2.get_size());
+    EXPECT_TRUE(test_vector.get_capacity() == test_vector2.get_capacity());
+    for (int i = 0; i < 8; i++) {
+        EXPECT_TRUE(*(test_vector.get_data() + i) == *(test_vector2.get_data() + i));
+    }
 }
